0a8e56abaa15f81868be2489f0b1f012103b0e10
Stephen Vance <steve@vance.com>
2012-04-29 11:00:16 -0400

    Fix compilation issue but host test still fails.

--- src/main/java/com/vance/quest2012/WebRetriever.java
+++ src/main/java/com/vance/quest2012/WebRetriever.java
@@ -8,6 +8,7 @@ package com.vance.quest2012;
 public class WebRetriever {
     private String target;
     private String protocol;
+    private String host;
 
     public WebRetriever(String target) {
         this.target = target;
@@ -21,5 +22,9 @@ public class WebRetriever {
 
     public String getProtocol() {
         return protocol;
+    }
+
+    public String getHost() {
+        return host;
     }
 }
