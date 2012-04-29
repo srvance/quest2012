0d1bd8323befb6fb2cdb27d4d8e5becdd4b3fddb
Stephen Vance <steve@vance.com>
2012-04-29 09:11:06 -0400

    First test and first failure

---
+++ src/test/java/com/vance/quest2012/WebRetrieverTest.java
@@ -0,0 +1,15 @@
+package com.vance.quest2012;
+
+import org.junit.Test;
+
+/**
+ * Unit test for {@link WebRetriever}
+ *
+ * @author srvance
+ */
+public class WebRetrieverTest {
+    @Test
+    public void testWebRetriever() {
+        WebRetriever sut;
+    }
+}
